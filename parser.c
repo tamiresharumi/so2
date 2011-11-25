2bdab0ebe7b5800b7c28a983b3f0ff9b279df707
Felipe Coelho <fcoelho_9@hotmail.com>
2011-11-25 13:33:55 -0200

    parser.(h|c)

---
+++ parser.c
@@ -0,0 +1,6 @@
+#include "parser.h"
+
+struct job* build_job(char **commands, int num_commands)
+{
+}
+
---
+++ parser.h
@@ -0,0 +1,31 @@
+#ifndef PARSER_H
+#define PARSER_H
+
+struct redirection
+{
+	const char *source;
+	const char *destiny;
+};
+
+struct process
+{
+	char **argv;
+	pid_t pid;
+	int finished;
+	int stopped;
+	int status;
+
+	struct redirection *redirections;
+	int num_redirections;
+};
+
+struct job
+{
+	struct process *processes;
+	int num_processes;
+};
+
+struct job* build_job(char **commands, int num_commands);
+
+#endif 
+
